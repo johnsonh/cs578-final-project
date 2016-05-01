4826a7a44c91321ebca301a6c8df12f7c4ec4594
Hsieh <hsiehj@amazon.com>
2016-05-01 11:44:35 -0700

    Committing to push, can send JSON to front end

--- project/analyze/views.py
+++ project/analyze/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the analyze index.")
+    context = {
+        'stuff': "print me!",
+    }
+    return render(request, 'analyze/index.html', context)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % 67)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+"""
+def visualization(request, nodesJson, linksJson):
+    return RenderTemplate(nodesJson, linksJson)
+
+"""
