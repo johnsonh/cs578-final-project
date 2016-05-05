231b6ecbb5c3650975ef86f14a35ffd1ba487db9
mooredawn12 <dawnmoor@usc.edu>
2016-05-05 00:35:07 -0500

    Update parser.py

--- project/analyze/parser.py
+++ project/analyze/parser.py
@@ -17,9 +17,11 @@ def analyze(path):
   didfail_folder = '/root/didfail/toyapps'
   
   # delete any data in covert and didfail folders before starting
-  shutil.rmtree(covert_apk_path)
+  if os.path.isdir(cover_apk_path):
+    shutil.rmtree(covert_apk_path)
   os.makedirs(covert_apk_path)
-  shutil.rmtree(didfail_path)
+  if os.path.isdir(didfail_path):
+    shutil.rmtree(didfail_path)
   os.makedirs(didfail_path)
   for file in os.listdir(didfail_folder):
     if file.endswith(".apk"):
